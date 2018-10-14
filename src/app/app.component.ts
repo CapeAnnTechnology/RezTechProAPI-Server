7cefdcdabed408cef12616f1005c5380379eb791
Bradmkjr <brad@ucanbonline.net>
2018-10-14 12:21:43 -0400

    Working version of chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'cat-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'RezTechProVIP';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+  private initModel(): void {
+  }
 }
