5a96ba8e3d75270df4aa9964082376987839cd89
Bradmkjr <brad@ucanbonline.net>
2018-11-16 16:56:38 -0500

    SSL Version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   SSO_URI: 'https://sso.reztechpro.com/',
   API_URI: 'https://reztechpro-api.herokuapp.com/v3.0/',
   BASE_API: 'https://reztechpro-api.herokuapp.com/v3.0/',
-  BASE_URI: 'http://reztech.us',
+  BASE_URI: 'https://reztech.us',
   AUTH0_CLIENT_DOMAIN: 'reztechpro.auth0.com',
   AUTH0_CLIENT_ID: 'SJLqm1Itq4vSMmc8NhEl59AgV5zVAHTu',
   NAMESPACE: 'https://reztechpro.com'
